69b3af70db034ec6968aa60196ff00ba8479af03
Arissa <ari.ssa@hotmail.com>
2025-02-17 19:51:24 +0000

    add `/api/v1/status` endpoint

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,4 @@ function Home() {
   return <h1>Boas-vindas! 😊</h1>;
 }
 
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("identação errada");
-}
-
 export default Home;
